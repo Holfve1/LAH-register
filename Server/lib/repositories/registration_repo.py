b1af9b9047d17c88be0d69e9519cbbec64b90532
chris <chris.holfve@hotmail.com>
2025-11-03 13:31:23 +0000

    creates base registration repo and tests

---
+++ Server/lib/repositories/registration_repo.py
@@ -0,0 +1,26 @@
+from lib.models.registration import Registration
+
+class RegistrationRepository():
+    def __init__(self, connection):
+        self.connection = connection
+
+    def all(self):
+        rows = self.connection.execute('SELECT * FROM registrations')
+        registrations = []
+        for row in rows:
+            item = Registration(row['id'], row['attendee_id'], row['date_id'])
+            registrations.append(item)
+        return registrations
+
+    def create(self, registration):
+        self.connection.execute('INSERT INTO registrations (attendee_id, date_id) VALUES(%s, %s)', [registration.attendee_id, registration.date_id])
+        return None
+    
+    def delete(self, id):
+        self.connection.execute('DELETE FROM registrations WHERE id = %s', [id])
+        return None
+    
+    def update(self, id, attendee_id, date_id):
+        self.connection.execute('UPDATE registrations SET attendee_id = %s, date_id = %s WHERE id = %s', [attendee_id, date_id, id])
+        return None
+    
---
+++ Server/tests/test_repositories/test_registartion_repo.py
@@ -0,0 +1,58 @@
+
+from lib.models.registration import Registration
+from lib.repositories.registration_repo import RegistrationRepository
+
+def test_gets_all_registrations(db_connection):
+    db_connection.seed("Server/seeds/seed.sql")
+    registration_repo = RegistrationRepository(db_connection)
+    registrations = registration_repo.all()
+    assert registrations == [
+                Registration(1, 1, 1),
+                Registration(2, 1, 2),
+                Registration(3, 1, 3),
+                Registration(4, 2, 1),
+                Registration(5, 3, 3),
+                Registration(6, 4, 2)
+    ]
+     
+def test_creates_registartions(db_connection):
+    db_connection.seed('Server/seeds/seed.sql')
+    registration_repo = RegistrationRepository(db_connection)
+    registration_repo.create(Registration(None, 3, 3))
+    registrations = registration_repo.all()
+    assert registrations == [
+                Registration(1, 1, 1),
+                Registration(2, 1, 2),
+                Registration(3, 1, 3),
+                Registration(4, 2, 1),
+                Registration(5, 3, 3),
+                Registration(6, 4, 2),
+                Registration(7, 3, 3)
+    ]
+
+def test_deletes_attendee(db_connection):
+    db_connection.seed('Server/seeds/seed.sql')
+    registration_repo = RegistrationRepository(db_connection)
+    registration_repo.delete(1)
+    registrations = registration_repo.all()
+    assert registrations == [
+                Registration(2, 1, 2),
+                Registration(3, 1, 3),
+                Registration(4, 2, 1),
+                Registration(5, 3, 3),
+                Registration(6, 4, 2)
+    ]
+
+def test_updates_attendee(db_connection):
+    db_connection.seed('Server/seeds/seed.sql')
+    registration_repo = RegistrationRepository(db_connection)
+    registration_repo.update(1, 3, 3)
+    registrations = registration_repo.all()
+    assert registrations == [
+                Registration(2, 1, 2),
+                Registration(3, 1, 3),
+                Registration(4, 2, 1),
+                Registration(5, 3, 3),
+                Registration(6, 4, 2),
+                Registration(1, 3, 3)
+    ]
