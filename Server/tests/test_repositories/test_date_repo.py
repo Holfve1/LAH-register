bbd77f211cd1694739dee70ed9ac1c73b80875f2
waseem <waseemrehman88@gmail.com>
2025-11-07 11:32:50 +0000

    create tests for date repo

--- Server/tests/test_repositories/test_date_repo.py
+++ Server/tests/test_repositories/test_date_repo.py
@@ -1,3 +1,5 @@
+
+from datetime import date
 from lib.models.date import Date
 from lib.repositories.date_repository import DateRepository
 
@@ -6,10 +8,12 @@ def test_gets_all_dates(db_connection):
     date_repo = DateRepository(db_connection)
     dates = date_repo.all()
     assert dates == [
-        Date(1, "2025-01-15", 1),
-        Date(2, "2025-02-20", 2),
-        Date(3, "2025-03-25", 3),
-        Date(4, "2025-04-30", 4),
+        Date(1, date(2025, 5, 3), 1),
+        Date(2, date(2025, 5, 5), 2),
+        Date(3, date(2025, 6, 11), 2),
+        Date(4, date(2025, 7, 15), 3),
+        Date(5, date(2025, 9, 21), 3),
+        Date(6, date(2025, 10, 26), 1),
     ]
 
 def test_creates_date(db_connection):
@@ -18,11 +22,13 @@ def test_creates_date(db_connection):
     date_repo.create(Date(None, "2025-05-10", 1))
     dates = date_repo.all()
     assert dates == [
-        Date(1, "2025-01-15", 1),
-        Date(2, "2025-02-20", 2),
-        Date(3, "2025-03-25", 3),
-        Date(4, "2025-04-30", 4),
-        Date(5, "2025-05-10", 1)
+       Date(1, date(2025, 5, 3), 1),
+        Date(2, date(2025, 5, 5), 2),
+        Date(3, date(2025, 6, 11), 2),
+        Date(4, date(2025, 7, 15), 3),
+        Date(5, date(2025, 9, 21), 3),
+        Date(6, date(2025, 10, 26), 1),
+        Date(7, date(2025, 5, 10), 1),
     ]
 
 def test_deletes_date(db_connection):
@@ -31,9 +37,11 @@ def test_deletes_date(db_connection):
     date_repo.delete(1)
     dates = date_repo.all()
     assert dates == [
-        Date(2, "2025-02-20", 2),
-        Date(3, "2025-03-25", 3),
-        Date(4, "2025-04-30", 4),
+        Date(2, date(2025, 5, 5), 2),
+        Date(3, date(2025, 6, 11), 2),
+        Date(4, date(2025, 7, 15), 3),
+        Date(5, date(2025, 9, 21), 3),
+        Date(6, date(2025, 10, 26), 1),
     ]
 
 def test_updates_date(db_connection):
@@ -42,17 +50,19 @@ def test_updates_date(db_connection):
     date_repo.update(1, "2025-01-20", 2)
     dates = date_repo.all()
     assert dates == [
-        Date(2, "2025-02-20", 2),
-        Date(3, "2025-03-25", 3),
-        Date(4, "2025-04-30", 4),
-        Date(1, "2025-01-20", 2),
+        Date(2, date(2025, 5, 5), 2),
+        Date(3, date(2025, 6, 11), 2),
+        Date(4, date(2025, 7, 15), 3),
+        Date(5, date(2025, 9, 21), 3),
+        Date(6, date(2025, 10, 26), 1),
+        Date(1, date(2025, 1, 20), 2),
     ]
 
 def test_find_by_date_id(db_connection):
     db_connection.seed('Server/seeds/seed.sql')
     date_repo = DateRepository(db_connection)
-    date = date_repo.find_by_date_id(2)
-    assert date == Date(2, "2025-02-20", 2)
+    the_date = date_repo.find_by_date_id(2)
+    assert the_date == Date(2, date(2025, 5, 5), 2)
 
     missing_date = date_repo.find_by_date_id(999)
     assert missing_date is None
@@ -62,7 +72,7 @@ def test_find_by_activity_id(db_connection):
     db_connection.seed('Server/seeds/seed.sql')
     date_repo = DateRepository(db_connection)
     dates = date_repo.find_by_activity_id(3)
-    assert dates == [Date(3, "2025-03-25", 3)]
-    
+    assert dates == [Date(4, date(2025, 7, 15), 3), Date(5, date(2025, 9, 21), 3)]
+
     no_dates = date_repo.find_by_activity_id(999)
     assert no_dates == []
