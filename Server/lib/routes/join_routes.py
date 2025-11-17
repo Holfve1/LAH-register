597eb2cd4fb5d38fd9bdab815f17eacaa05f6538
waseem <waseemrehman88@gmail.com>
2025-11-17 16:44:02 +0000

    create join routes

---
+++ Server/lib/routes/join_routes.py
@@ -0,0 +1,98 @@
+from flask import Flask, jsonify, request
+from lib.database_connection import get_flask_database_connection
+from lib.repositories.join_repo import JoinRepository
+
+def apply_join_routes(app):
+
+    @app.route('/activities-by-attendee', methods=['GET'])
+    def get_all_activities_by_attendee_id():
+        connection = get_flask_database_connection(app)
+        join_repo = JoinRepository(connection)
+        attendee_id = request.args.get('attendee_id')
+        activities = join_repo.find_activities_by_attendee_id(attendee_id)
+        result = []
+        for activity, date in activities:
+            result.append({
+                'activity': activity,
+                'date': date
+            })
+        return jsonify(result)
+    
+    @app.route('/attendees-and-activities', methods=['GET'])
+    def get_all_attendees_and_their_activities():
+        connection = get_flask_database_connection(app)
+        join_repo = JoinRepository(connection)
+        rows = join_repo.all_attendees_with_activities()
+        result = []
+        for row in rows:
+            result.append({
+                'first_name': row['first_name'],
+                'last_name': row['last_name'],
+                'activity': row['activity'],
+            })
+        return jsonify(result)
+    
+    @app.route('/dates-and-attendees-by-activity', methods=['GET'])
+    def get_dates_and_attendees_by_activity_id():
+        connection = get_flask_database_connection(app)
+        join_repo = JoinRepository(connection)
+        activity_id = request.args.get('activity_id')
+        rows = join_repo.find_dates_and_attendees_by_activity_id(activity_id)
+        result = []
+        for row in rows:
+            result.append({
+                'date': row['date'],
+                'first_name': row['first_name'],
+                'last_name': row['last_name']
+            })
+        return jsonify(result)
+    
+    @app.route('/activities-with-dates-and-attendees', methods=['GET'])
+    def get_all_activities_with_dates_and_attendees():
+        connection = get_flask_database_connection(app)
+        join_repo = JoinRepository(connection)
+        rows = join_repo.all_activities_with_dates_and_attendees()
+        result = []
+        for row in rows:
+            result.append({
+                'date': row['date'],
+                'activity': row['activity'],
+                'first_name': row['first_name'],
+                'last_name': row['last_name']
+            })
+        return jsonify(result)
+
+    @app.route('/dates-with-activities-and-attendees', methods=['GET'])
+    def get_a_date_with_activities_and_attendees():
+        connection = get_flask_database_connection(app)
+        join_repo = JoinRepository(connection)
+        date_id = request.args.get('date_id')
+        rows = join_repo.find_date_with_activities_and_attendees(date_id)
+        result = []
+        for row in rows:
+            result.append({
+                'date': row['date'],
+                'activity': row['activity'],
+                'first_name': row['first_name'],
+                'last_name': row['last_name']
+            })
+        return jsonify(result)
+    
+    @app.route('/all-dates-with-activities-and-attendees', methods=['GET'])
+    def get_all_dates_with_activities_and_attendees():
+        connection = get_flask_database_connection(app)
+        join_repo = JoinRepository(connection)
+        rows = join_repo.all_dates_with_activities_and_attendees()
+        result = []
+        for row in rows:
+            result.append({
+                'date': row['date'],
+                'activity': row['activity'],
+                'first_name': row['first_name'],
+                'last_name': row['last_name']
+            })
+        return jsonify(result)
+
+
+
+        
