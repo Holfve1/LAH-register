3c211fc67602a04060f9fb23ff471818b6d7d466
chris <chris.holfve@hotmail.com>
2025-11-17 14:48:03 +0000

    add registration routes and tests

--- Server/app.py
+++ Server/app.py
@@ -5,7 +5,7 @@ from lib.routes.attendee_routes import apply_attendee_routes
 from lib.routes.activity_routes import apply_activity_routes
 from lib.routes.csv_routes import apply_csv_routes
 from lib.routes.date_routes import apply_date_routes
-
+from lib.routes.registration_routes import apply_registration_routes
 # Create a new Flask app
 app = Flask(__name__)
 app.secret_key = 'i_dont_get_this'
@@ -14,6 +14,7 @@ app.secret_key = 'i_dont_get_this'
 apply_attendee_routes(app)
 apply_activity_routes(app)
 apply_date_routes(app)
+apply_registration_routes(app)
 
 apply_csv_routes(app)
 
---
+++ Server/lib/routes/registration_routes.py
@@ -0,0 +1,59 @@
+from flask import Flask, jsonify, request
+from lib.database_connection import get_flask_database_connection
+from lib.repositories.registration_repo import RegistrationRepository
+from lib.models.registration import Registration
+
+
+def apply_registration_routes(app):
+    
+
+    @app.route('/registrations', methods=['GET'])
+    def get_all_registrations():
+        connection = get_flask_database_connection(app)        
+        registration_repo = RegistrationRepository(connection)
+        registrations = registration_repo.all()
+        result = []
+        for registration in registrations:
+            result.append({
+                'id': registration.id,
+                'attendee_id': registration.attendee_id,
+                'date_id': registration.date_id,
+            })
+        return jsonify(result)
+    
+    @app.route('/registrations', methods=['POST'])
+    def create_registration():
+        connection = get_flask_database_connection(app)
+        registration_repo = RegistrationRepository(connection)
+        data = request.get_json()
+        date_id = data.get('date_id')
+        attendee_id = data.get('attendee_id')
+        new_registration = Registration(None, date_id, attendee_id)
+        registration_repo.create(new_registration)
+        return jsonify({
+            'message': 'Registration created successfully',
+            'registration': {
+                'id': new_registration.id,
+                'attendee_id': new_registration.attendee_id,
+                'date_id': new_registration.date_id
+            }
+        }), 201
+    
+    @app.route('/registrations/<int:id>', methods=['DELETE'])
+    def delete_registration(id):
+        connection = get_flask_database_connection(app)
+        registration_repo = RegistrationRepository(connection)
+        registration_repo.delete(id)
+        return '', 204 
+    
+    @app.route('/registrations/<int:id>', methods=['PATCH'])
+    def update_registration(id):
+        connection = get_flask_database_connection(app)
+        registration_repo = RegistrationRepository(connection)
+        data = request.get_json()
+        date_id = data.get('date_id')
+        attendee_id = data.get('attendee_id')
+        registration_repo.update(id, attendee_id, date_id)
+        return jsonify({
+            'message': 'Registration updated successfully',}
+        ), 200
