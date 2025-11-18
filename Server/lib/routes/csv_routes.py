00bce6c3bcc1ec6e5ea36650808400642a728a5c
chris <chris.holfve@hotmail.com>
2025-11-18 11:36:01 +0000

    add csv routes and tests

--- Server/lib/routes/csv_routes.py
+++ Server/lib/routes/csv_routes.py
@@ -1,4 +1,4 @@
-from flask import Response
+from flask import Response, request
 import csv
 from io import StringIO
 from lib.database_connection import get_flask_database_connection
@@ -6,6 +6,7 @@ from lib.repositories.activity_repo import ActivityRepository
 from lib.repositories.attendee_repo import AttendeeRepository
 from lib.repositories.date_repo import DateRepository
 from lib.repositories.registration_repo import RegistrationRepository
+from lib.repositories.join_repo import JoinRepository
 
 def apply_csv_routes(app):
 
@@ -81,3 +82,116 @@ def apply_csv_routes(app):
             headers={'Content-Disposition': 'attachment; filename=registrations.csv'}
         )
     
+    @app.route('/attendee-activities/export', methods=['GET'])
+    def export_attendee_activities_to_csv():
+        connection = get_flask_database_connection(app)
+        join_repo = JoinRepository(connection)
+        attendee_id = request.args.get('id')
+        rows = join_repo.find_activities_by_attendee_id(attendee_id)
+        buffer = StringIO() 
+        writer = csv.writer(buffer)
+        writer.writerow(['activity', 'date'])
+        for row in rows:
+            writer.writerow([row['activity'], row['date']])
+        csv_data = buffer.getvalue()
+        buffer.close()
+        return Response(
+            csv_data,
+            mimetype='text/csv',
+            headers={'Content-Disposition': 'attachment; filename=attendee_activities.csv'}
+        )
+
+    @app.route('/activities-attendees/export', methods=['GET'])
+    def export_activities_and_attendees_to_csv():
+        connection = get_flask_database_connection(app)
+        join_repo = JoinRepository(connection)
+        rows = join_repo.all_attendees_with_activities()
+        buffer = StringIO() 
+        writer = csv.writer(buffer)
+        writer.writerow(['activity', 'first_name', 'last_name'])
+        for row in rows:
+            writer.writerow([row['activity'], row['first_name'], row['last_name']])
+        csv_data = buffer.getvalue()
+        buffer.close()
+        return Response(
+            csv_data,
+            mimetype='text/csv',
+            headers={'Content-Disposition': 'attachment; filename=activities_attendees.csv'}
+        )
+
+    @app.route('/activity-dates-attendees/export', methods=['GET'])
+    def export_activity_dates_attendees_to_csv():
+        connection = get_flask_database_connection(app)
+        join_repo = JoinRepository(connection)
+        activity_id = request.args.get('id')
+        rows = join_repo.find_dates_and_attendees_by_activity_id(activity_id)
+        buffer = StringIO()
+        writer = csv.writer(buffer)
+        writer.writerow(['date', 'first_name', 'last_name'])
+        for row in rows:
+            writer.writerow([row['date'], row['first_name'], row['last_name']])
+        csv_data = buffer.getvalue()
+        buffer.close()
+        return Response(
+            csv_data,
+            mimetype = 'text/csv',
+            headers={'Content-Disposition': 'attachment; filename=activity-dates-attendees.csv'}
+        )
+
+    @app.route('/activities-dates-attendees/export', methods=['GET'])
+    def export_activities_dates_attendees_to_csv():
+        connection = get_flask_database_connection(app)
+        join_repo = JoinRepository(connection)
+        rows = join_repo.all_activities_with_dates_and_attendees()
+        buffer = StringIO()
+        writer = csv.writer(buffer)
+        writer.writerow(['date', 'activity', 'first_name', 'last_name'])
+        for row in rows:
+            writer.writerow([row['date'], row['activity'], row['first_name'], row['last_name']])
+        csv_data = buffer.getvalue()
+        buffer.close()
+        return Response(
+            csv_data,
+            mimetype='text/csv',
+            headers={'Content-Disposition': 'attachment; filename=activities-dates-attendees.csv'}
+        )
+
+    @app.route('/date-activities-attendees/export', methods=['GET'])
+    def export_date_activities_attendees_to_csv():
+        connection = get_flask_database_connection(app)
+        join_repo = JoinRepository(connection)
+        date_id = request.args.get('id')
+        rows = join_repo.find_date_with_activities_and_attendees(date_id)
+        buffer = StringIO()
+        writer = csv.writer(buffer)
+        writer.writerow(['date', 'activity', 'first_name', 'last_name'])
+        for row in rows:
+            writer.writerow([row['date'], row['activity'], row['first_name'], row['last_name']])
+        csv_data = buffer.getvalue()
+        buffer.close()
+        return Response(
+            csv_data,
+            mimetype='text/csv',
+            headers={'Content-Disposition': 'attachment; filename=date-activities-attendees.csv'}
+        )
+
+    @app.route('/dates-activities-attendees/export', methods=['GET'])
+    def export_dates_activities_attendees_to_csv():
+        connection = get_flask_database_connection(app)
+        join_repo = JoinRepository(connection)
+        rows = join_repo.all_dates_with_activities_and_attendees()
+        buffer = StringIO()
+        writer = csv.writer(buffer)
+        writer.writerow(['date', 'activity', 'first_name', 'last_name'])
+        for row in rows:
+            writer.writerow([row['date'], row['activity'], row['first_name'], row['last_name']])
+        csv_data = buffer.getvalue()
+        buffer.close()
+        return Response(
+            csv_data,
+            mimetype='text/csv',
+            headers={'Content-Disposition': 'attachment; filename=dates-activities-attendees.csv'}
+        )
+
+
+
