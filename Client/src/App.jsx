477c85de27371445ffcd8a2c2a6ed67af4ad3f98
chris <chris.holfve@hotmail.com>
2025-12-02 10:51:06 +0000

    updating set up

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,25 @@
-export default function App() {
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
+import { ViewPage } from "./pages/ViewPage";
+
+const router = createBrowserRouter([
+
+    {path: "/", element: <LoginPage />,},
+    {path: "/login", element: <LoginPage />,},
+    {path: "/register", element: <RegisterPage />,},
+    {path: "/view", element: <ViewPage />,},
+    
+]);
+
+function App() {
   return (
-    <div style={{ padding: 24, fontFamily: 'system-ui, sans-serif' }}>
-      <h1>LAH Frontend</h1>
-      <p>If you can see this, the Vite + React setup is working.</p>
-    </div>
-  )
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
+export default App;
