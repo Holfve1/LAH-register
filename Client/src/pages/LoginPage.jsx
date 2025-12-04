a7d79c7e76fe2a8d952f7e2fa50ffcac75d8f63f
waseem <waseemrehman88@gmail.com>
2025-12-04 13:41:42 +0000

    fix tailwind setup issue and test styling on login page

--- Client/src/pages/LoginPage.jsx
+++ Client/src/pages/LoginPage.jsx
@@ -30,15 +30,29 @@ export function LoginPage() {
 
   return (
     <main className="min-h-screen flex flex-col items-center justify-center px-4">
-      <h2>Admin Login</h2>
-      <form onSubmit={handleLogin}>
+      <h2 className="text-3xl text-white font-semibold mb-6">Admin Login</h2>
+      <form 
+        onSubmit={handleLogin}
+        className="flex flex-col gap-4 w-full max-w-xs items-center">
         <input
           type="password"
           placeholder="Enter admin password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          className="w-full p-3 rounded bg-white text-black placeholder-gray-500 shadow focus:outline-none focus:ring-2 focus:ring-[#ea5136]"
         />
-        <button type="submit">Login</button>
+        <button 
+        type="submit"
+        className="p-3 w-fit rounded bg-[#ea5136] text-white font-medium hover:bg-[#d1442d] transition"
+        >
+          Login
+        </button>
+        <button 
+        type="submit"
+        className="p-3 w-fit rounded text-white font-medium hover:transition"
+        >
+          Forgot Password
+        </button>
       </form>
     </main>
   );
