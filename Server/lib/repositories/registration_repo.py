f6281cac60fe4eb8a9baec87f9d95e140254ed61
waseem <waseemrehman88@gmail.com>
2026-01-21 16:31:49 +0000

    Fix database issues realting to duplicate ids when registering and attendee

--- Server/lib/repositories/registration_repo.py
+++ Server/lib/repositories/registration_repo.py
@@ -13,8 +13,16 @@ class RegistrationRepository():
         return registrations
 
     def create(self, registration):
-        self.connection.execute('INSERT INTO registrations (attendee_id, date_id) VALUES(%s, %s)', [registration.attendee_id, registration.date_id])
-        return None
+        rows = self.connection.execute(
+            '''
+            INSERT INTO registrations (attendee_id, date_id)
+            VALUES (%s, %s)
+            RETURNING id
+            ''',
+            [registration.attendee_id, registration.date_id]
+        )
+        registration.id = rows[0]['id']
+        return registration
     
     def delete(self, id):
         self.connection.execute('DELETE FROM registrations WHERE id = %s', [id])
@@ -24,3 +32,15 @@ class RegistrationRepository():
         self.connection.execute('UPDATE registrations SET attendee_id = %s, date_id = %s WHERE id = %s', [attendee_id, date_id, id])
         return None
     
+    def find_by_attendee_and_date(self, attendee_id, date_id):
+        rows = self.connection.execute(
+            '''
+            SELECT * FROM registrations
+            WHERE attendee_id = %s AND date_id = %s
+            ''',
+            [attendee_id, date_id]
+        )
+        if rows:
+            row = rows[0]
+            return Registration(row['id'], row['attendee_id'], row['date_id'])
+        return None
