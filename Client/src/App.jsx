1098a961b984476e6fabc928b12edeb2432cc5c5
chris <chris.holfve@hotmail.com>
2026-01-22 13:05:16 +0000

    adds footer and header with logos

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -17,17 +17,22 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <>
+    <>      
+      <header className="header-bar">
+        <img src="/LAH_Logo.png" alt="LAH" className="header-logo" />
+      </header>
+
       <RouterProvider router={router} />
+
       <video
         className="bg-video"
-        src="/CW_Logo_orange.mp4"
+        src="/logo.mp4"
         autoPlay
         muted
         loop
       />
+      <div className="footer-bar" />
     </>
-    
   );
 }
 
